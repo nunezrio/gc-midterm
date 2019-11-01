e1cfecbf8b23cbaa19469520866976723f1b6e9d
Casey <clounsbur@gmail.com>
2019-11-01 10:43:43 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+class Budget {
+  constructor(amount) {
+    this.weeklyBudget = amount;
+    // this.bills = bills;  INstead of these, add Rion's array stuff
+    // this.clothes = clothes;
+    // this.food = food;
+    // this.entertainment = entertainment;
+  }
+}
+function getBudgetAmt(e) {
+  e.preventDefault();
+  console.log("function ran!");
+  let budgetAmt = document.getElementById("weeklyBudget");
+
+  if (budgetAmt.value > 0) {
+    let elMsg = document.querySelector(".displayBudget");
+    elMsg.innerText = `You have ${budgetAmt} left to spend this week.`;
+  } else elMsg = `You have no money left  you are broke. Stop spending money`;
+
+  // Alicia, put your code here :D
+}
+let budget = document.querySelector(".budget");
+budget.addEventListener("submit", getBudgetAmt);
+
+// function calcTotal () {
+//     let budgetInput = document.querySelector(".budget").weeklyBudget.value;
+//     budgetInput - (this.bills + this.clothes + this.food + this.entertainment)
+//
+// }
+
+// }
